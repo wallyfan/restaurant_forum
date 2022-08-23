f4dddfd390ba0de2060ac7a34f6d686dc3104c2c
wallyfan <107678451+wallyfan@users.noreply.github.com>
2022-08-23 19:55:15 +0800

    create category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_21_113744) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_23_115009) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "tel"
@@ -20,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_21_113744) do
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
     t.string "image"
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
