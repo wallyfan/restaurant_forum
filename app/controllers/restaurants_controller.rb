a5202f5cc3bf5eea17cc0e93cb77f0402231d887
wallyfan <107678451+wallyfan@users.noreply.github.com>
2022-08-23 11:34:15 +0800

    index前台顯示

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,23 @@
 class RestaurantsController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :set_restaurant, only: [:show]
 
   def index
+      @restaurants = Restaurant.page(params[:page]).per(10)
+  end
+
+  def show
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description, :image)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
  
   root "restaurants#index"
+  
+  get "/:id", to: "restaurants#show"
 
   namespace :admin do
     resources :restaurants
