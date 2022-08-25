589e2d04beead4a0125cb3c07f0a0134b139a857
wallyfan <107678451+wallyfan@users.noreply.github.com>
2022-08-25 10:26:02 +0800

    user can update category with the same form on index page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,21 +4,37 @@ class Admin::CategoriesController < ApplicationController
   before_action :authenticate_admin
 
   def index
-    @categories = Category.all
-    @category = Category.new
+  @categories = Category.all
+    if params[:id]
+      @category = Category.find(params[:id])
+    else
+      @category = Category.new
+    end
   end
 
   def create
   @category = Category.new(category_params)
-  if @category.save
-    flash[:notice] = "新餐廳分類已建立"
-    redirect_to admin_categories_path
+    if @category.save
+      flash[:notice] = "新餐廳分類已建立"
+      redirect_to admin_categories_path
 
+    else
+      @categories = Category.all
+      render :index
+    end
+  end
+
+def update
+  @category = Category.find(params[:id])
+  if @category.update(category_params)
+    redirect_to admin_categories_path
+    flash[:notice] = "餐廳分類名稱修改成功"
   else
     @categories = Category.all
     render :index
   end
 end
+
 
 private
 
