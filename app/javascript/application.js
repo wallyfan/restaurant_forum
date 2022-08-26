f59f85fc327856365460b46cd9ff0a08f724db29
wallyfan <107678451+wallyfan@users.noreply.github.com>
2022-08-26 17:40:31 +0800

    admin can delete comment

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import React from "react"
 import ReactDOM from "react-dom"
+
+//= require_tree
+//= require jquery
+//= require jquery_ujs
