ad5a330b3416b44b3d5c599d0b476a960298fe53
wallyfan <107678451+wallyfan@users.noreply.github.com>
2022-08-29 11:07:13 +0800

    setup route for user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
- 
+  
+  resources :users, only: [:show, :edit, :update]
+
   resources :restaurants, only: [:index, :show] do
     resources :comments, only: [:create, :destroy]
   end
