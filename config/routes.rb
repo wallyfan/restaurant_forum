5d9d020888b2bd99a9e8a6605bbdd30731e837ea
wallyfan <107678451+wallyfan@users.noreply.github.com>
2022-08-30 11:51:40 +0800

    setup routes for feeds and dashboard pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   resources :users, only: [:show, :edit, :update]
 
   resources :restaurants, only: [:index, :show] do
     resources :comments, only: [:create, :destroy]
+
+    # 瀏覽所有餐廳的最新動態
+    collection do
+      get :feeds
+    end
+
+    # 瀏覽個別餐廳的 Dashboard
+    member do
+      get :dashboard
+    end
+    
   end
 
   resources :categories, only: :show
