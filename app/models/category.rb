f25f87e36b9dfe318e772daa1da9b262f0f44123
wallyfan <107678451+wallyfan@users.noreply.github.com>
2022-08-30 14:18:42 +0800

    create favorite model&setup relation among user, restaurant and favorite

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
   validates_presence_of :name
   validates_uniqueness_of :name
 
-  has_many :restaurants, dependent: :restrict_with_exception
+  has_many :restaurants, dependent: :restrict_with_error
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_030121) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_054538) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -21,6 +21,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_030121) do
     t.text "content"
     t.integer "restaurant_id"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "restaurant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
